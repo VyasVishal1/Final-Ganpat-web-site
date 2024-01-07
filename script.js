979da1983df6a9b84d1bdf92bc4063b7dbffa811
Vyas Vishal <147301831+VyasVishal1@users.noreply.github.com>
2024-01-07 15:48:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+let button = document.getElementById("button");
+
+
+        
+        button.addEventListener("click",()=>{
+           let nav = document.getElementById("JSNav")
+           nav.style.display = "flex";
+           nav.style.height = "80vh"
+           nav.style.animation = "show 2s"
+           
+            
+        });
+        
+        let up = document.getElementById("JSNav");
+        up.addEventListener("click",()=>{
+            let nav = document.getElementById("JSNav")
+           nav.style.animation = "Hidenav 2s"
+            
+            nav.style.height = "0vh"
+            
+        })
+
+
+
+
+
+let AddmissionButton = document.getElementById("Addmission");
+
+
+        
+        AddmissionButton.addEventListener("click",()=>{
+           let AddmissionDiv = document.getElementById("AddmissionDiv")
+           AddmissionDiv.style.display = "block";
+           
+           AddmissionDiv.style.animation = "AddmissionShow .5s forwards ease-out"
+           
+           
+            
+        });
+        
+        let close = document.getElementById("close");
+
+
+        
+        close.addEventListener("click",()=>{
+           let AddmissionDiv = document.getElementById("AddmissionDiv")
+           AddmissionDiv.style.display = "none";
+           AddmissionDiv.style.animation = "AddmissionHide .5s ease-out "
+           
+           
+            
+        });
+
+  
+  
+     var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 50,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+    var swiper2 = new Swiper(".mySwiper2", {
+      direction: "vertical",
+      spaceBetween: 50,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+
+
+
+
+ var swiper = new Swiper(".mySwiperTwo", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+    
+    
+    
+    
+    
+    
